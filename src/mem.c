017ce92f5b45703a4157cb1fa34963bdd76faf00
Aman <prasadaman2000@gmail.com>
2026-02-19 20:07:54 -0800

    some more refactoring

--- src/mem.c
+++ src/mem.c
@@ -2,7 +2,7 @@
 #include "../include/core.h"
 #include "../include/mem.h"
 
-#define WSRAM_ALLOCATOR (wsram_allocator *)0x02000000
+#define WRAM_ALLOCATOR (wram_allocator *)0x02000000
 #define ALLOC_BASE (void *) 0x02000100
 #define ALLOC_BLOCK_TRACK (block *) 0x02030000
 #define FREE_BLOCK_TRACK (block *) 0x0203F000
@@ -20,23 +20,36 @@ typedef struct block_allocator_t {
     uint32 num_free;
     block* alloc_block_track;
     block* free_block_track;
+    void* ceiling;
 } block_allocator;
 
-typedef struct wsram_allocator_t {
-    block_allocator wram_allocator;
+typedef struct wram_allocator_t {
+    block_allocator ewram_allocator;
     uint32 debug_value;
-} wsram_allocator;
+} wram_allocator;
 
-wsram_allocator* wsram_allocator_ptr() {
-    return WSRAM_ALLOCATOR;
+wram_allocator* wram_allocator_ptr() {
+    return WRAM_ALLOCATOR;
+}
+
+// must be called once at startup
+void mem_init() {
+    wram_allocator* w_a = wram_allocator_ptr();
+
+    w_a->ewram_allocator.alloc_block_track = ALLOC_BLOCK_TRACK;
+    w_a->ewram_allocator.free_block_track = FREE_BLOCK_TRACK;
+    w_a->ewram_allocator.next_alloc_location = ALLOC_BASE;
+    w_a->ewram_allocator.num_allocs = 0;
+    w_a->ewram_allocator.num_free = 0;
+    w_a->ewram_allocator.ceiling = WRAM_CEIL;
 }
 
 void _write_debug_value(uint32 val) {
-    wsram_allocator_ptr()->debug_value = val;
+    wram_allocator_ptr()->debug_value = val;
 }
 
 uint32 _read_debug_value() {
-    return wsram_allocator_ptr()->debug_value;
+    return wram_allocator_ptr()->debug_value;
 }
 
 uint32 _get_num_allocs(block_allocator* allocator) {
@@ -103,30 +116,20 @@ void _remove_alloced_block(block_allocator* allocator, uint32 alloc_block_idx) {
     _set_num_allocs(allocator, _get_num_allocs(allocator) - 1);
 }
 
-// must be called once at startup
-void mem_init() {
-    wsram_allocator* w_a = wsram_allocator_ptr();
-    w_a->wram_allocator.alloc_block_track = ALLOC_BLOCK_TRACK;
-    w_a->wram_allocator.free_block_track = FREE_BLOCK_TRACK;
-    w_a->wram_allocator.next_alloc_location = ALLOC_BASE;
-    w_a->wram_allocator.num_allocs = 0;
-    w_a->wram_allocator.num_free = 0;
-}
-
 void* malloc(uint32 size) {
-    block_allocator* wram_allocator = &(wsram_allocator_ptr()->wram_allocator);
+    block_allocator* ewram_allocator = &(wram_allocator_ptr()->ewram_allocator);
     if (size & 0b11) {
         size = (size - (size & 0b11)) + 0b100;
     }
     _write_debug_value(size);
-    uint32 num_free = _get_num_free(wram_allocator);
+    uint32 num_free = _get_num_free(ewram_allocator);
     void* addr = NULL;
     for (uint32 i = 0; i < num_free; ++i) {
-        block* free_block = wram_allocator->free_block_track + i;
+        block* free_block = ewram_allocator->free_block_track + i;
         if (free_block->size == size) {
             addr = free_block->location;
-            _write_allocated_block(wram_allocator, *free_block);
-            _remove_free_block(wram_allocator, i);
+            _write_allocated_block(ewram_allocator, *free_block);
+            _remove_free_block(ewram_allocator, i);
             return addr;
         }
         else if (free_block->size > size) {
@@ -134,19 +137,19 @@ void* malloc(uint32 size) {
             block new_block;
             new_block.location = addr;
             new_block.size = size;
-            _write_allocated_block(wram_allocator, new_block);
+            _write_allocated_block(ewram_allocator, new_block);
             free_block->location = (char*)free_block->location + size;
             free_block->size -= size;
             return addr;
         }
     }
 
-    if ((char*)(wram_allocator->next_alloc_location) + size < (char*)(wram_allocator->alloc_block_track)) {
+    if ((char*)(ewram_allocator->next_alloc_location) + size < (char*)(ewram_allocator->alloc_block_track)) {
         block new_block;
-        new_block.location = _get_alloc_location(wram_allocator);
+        new_block.location = _get_alloc_location(ewram_allocator);
         new_block.size = size;
-        _set_alloc_location(wram_allocator, (char*)new_block.location + size);
-        _write_allocated_block(wram_allocator, new_block);
+        _set_alloc_location(ewram_allocator, (char*)new_block.location + size);
+        _write_allocated_block(ewram_allocator, new_block);
         return new_block.location;
     }
     _panic();
@@ -154,12 +157,12 @@ void* malloc(uint32 size) {
 }
 
 void free(void* ptr) {
-    block_allocator* wram_allocator = &(wsram_allocator_ptr()->wram_allocator);
-    for (uint32 i = 0; i < _get_num_allocs(wram_allocator); ++i) {
-        block* alloced_block = wram_allocator->alloc_block_track + i;
+    block_allocator* ewram_allocator = &(wram_allocator_ptr()->ewram_allocator);
+    for (uint32 i = 0; i < _get_num_allocs(ewram_allocator); ++i) {
+        block* alloced_block = ewram_allocator->alloc_block_track + i;
         if (alloced_block->location == ptr) {
-            _write_free_block(wram_allocator, *alloced_block);
-            _remove_alloced_block(wram_allocator, i);
+            _write_free_block(ewram_allocator, *alloced_block);
+            _remove_alloced_block(ewram_allocator, i);
             return;
         }
     }
